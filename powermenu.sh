e17d406d5566ca92a502e6176ab3acb7405e9b6c
Hannes <hannes10042004@icloud.com>
2023-10-20 10:10:20 +0200

    Update powermenu.sh

--- powermenu.sh
+++ powermenu.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/bash
 #
-chosen=$(printf "󰐥 Power Off\n Restart\n Log out" | rofi -dmenu -i -l 3)
+chosen=$(printf "󰐥 Power Off\n Restart\n󰤄 Suspend\n Log out" | rofi -dmenu -i -l 3)
 case "$chosen" in 
 	"󰐥 Power Off") shutdown now;;
 	" Restart") reboot ;;
+     "󰤄 Suspend") systemctl suspend ;;
     " Log out") i3-msg exit ;;
 	*) exit 1 ;;
 esac
